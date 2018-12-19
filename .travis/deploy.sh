40034957dd75af09693e0e6ee878d3529480e7a6
Michael Hollister <hollister.michael@gmail.com>
2018-12-19 00:24:57 -0600

    :)

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,7 +2,7 @@
 # stop the service
 ssh ${REMOTE_USER}@${REMOTE_SERVER} "/usr/bin/sudo systemctl stop home-api-node"
 # scp the binary
-scp ./bin/http-proxy ${REMOTE_USER}@${REMOTE_SERVER}:/usr/local/bin/home-api-node
+scp ./bin/home-api-node ${REMOTE_USER}@${REMOTE_SERVER}:/usr/local/bin/home-api-node
 ssh ${REMOTE_USER}@${REMOTE_SERVER} "/bin/chmod +x /usr/local/bin/home-api-node"
 # start the service
 ssh ${REMOTE_USER}@${REMOTE_SERVER} "/usr/bin/sudo systemctl start home-api-node"
