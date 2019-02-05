05a05743c8ca52c8da3bfdb11754af10e4160982
Mike Hollister <hollister.michael@gmail.com>
2019-02-05 14:24:10 -0600

    quiet logs

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # stop the service
-ssh ${REMOTE_USER}@${REMOTE_SERVER} "/usr/bin/sudo systemctl stop home-api-node"
+ssh -o LogLevel=quiet ${REMOTE_USER}@${REMOTE_SERVER} "/usr/bin/sudo systemctl stop home-api-node"
 # scp the binary
-scp ./bin/home-api-node ${REMOTE_USER}@${REMOTE_SERVER}:/usr/local/bin/home-api-node
-ssh ${REMOTE_USER}@${REMOTE_SERVER} "/bin/chmod +x /usr/local/bin/home-api-node"
+scp -o LogLevel=quiet ./bin/home-api-node ${REMOTE_USER}@${REMOTE_SERVER}:/usr/local/bin/home-api-node
+ssh -o LogLevel=quiet ${REMOTE_USER}@${REMOTE_SERVER} "/bin/chmod +x /usr/local/bin/home-api-node"
 # start the service
-ssh ${REMOTE_USER}@${REMOTE_SERVER} "/usr/bin/sudo systemctl start home-api-node"
+ssh -o LogLevel=quiet ${REMOTE_USER}@${REMOTE_SERVER} "/usr/bin/sudo systemctl start home-api-node"
