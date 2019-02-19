eef0bcfd9c4dcfa0ac71c71a483a4c7b2ead7828
Mike Hollister <hollister.michael@gmail.com>
2019-02-19 11:36:02 -0600

    removed deployment files

--- .travis/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# stop the service
-ssh -o LogLevel=quiet ${REMOTE_USER}@${REMOTE_SERVER} "/usr/bin/sudo systemctl stop aviary-api"
-# scp the binary
-scp -o LogLevel=quiet ./bin/aviary-api ${REMOTE_USER}@${REMOTE_SERVER}:/usr/local/bin/aviary-api
-ssh -o LogLevel=quiet ${REMOTE_USER}@${REMOTE_SERVER} "/bin/chmod +x /usr/local/bin/aviary-api"
-# start the service
-ssh -o LogLevel=quiet ${REMOTE_USER}@${REMOTE_SERVER} "/usr/bin/sudo systemctl start aviary-api"
