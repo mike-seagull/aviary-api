0dc3053a88fa471469db30098f3fce202837893d
Mike Hollister <hollister.michael@gmail.com>
2022-07-07 21:59:37 +0000

    added tinyurl clone

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI, Request, Form
+from fastapi import FastAPI, Request, Form, HTTPException
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 import os.path
 import markdown
+import hashlib
+import random
 # from .routers.cook import router as cook_router
 
 app = FastAPI()
@@ -14,7 +16,7 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return RedirectResponse("/docs")
 
 @app.get("/page/{page_name}", response_class=HTMLResponse)
 async def show_page(request: Request, page_name: str):
@@ -36,3 +38,41 @@ def cook_page(request: Request):
 def convert(request: Request, orig_temp: int = Form(...), orig_time_in_mins: int = Form(...),  new_temp: int = Form(...),):
     new_time = orig_time_in_mins * (orig_temp / new_temp)
     return templates.TemplateResponse('cook.html', context={'request': request, "new_temp": new_temp, 'new_time': int(new_time)})
+
+urls = {}
+tags = {}
+@app.get("/rd")
+def redirect_page(request: Request):
+    return templates.TemplateResponse(name="redirect.html", context={'request': request})
+
+@app.post("/rd", response_class=HTMLResponse)
+def create_tag(request: Request, url: str = Form(...) ):
+    
+    if url not in urls:
+        # generate tag
+        tag = ""
+        while 1:
+            TAG_LENGTH = 6
+            tag = list(url)
+            random.shuffle(tag)
+            tag = "".join(tag)
+            tag = hashlib.md5(tag.encode("utf-8")).hexdigest()[:TAG_LENGTH] # generate tags
+            print(tag)
+            if tag not in tags:
+                # add url and tag to db
+                tags[tag] = url
+                urls[url] = tag
+                break
+    else: # use the old tag
+        tag = urls[url]
+    return templates.TemplateResponse(name="redirect.html", context={'request': request, "tag": tag})
+
+@app.get("/rd/{tag}")
+def redirect(tag: str, request: Request):
+    # connect to db
+    # return 500 if no tag is found
+    try:
+        url = tags[tag] # get url from tag
+        return RedirectResponse(url)
+    except KeyError:
+        return HTTPException(status_code=404, detail="Invalid URL")
